050c61d7329587e5aedc2fb8e6944eae93c71656
Juan J Garcia <gtechjg@gmail.com>
2024-10-10 20:21:55 +0000

    my additions to code - "auto" keyword

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+//cout stands for character output
+#include <iostream>
+auto main() -> int{
+    std::cout << "Hello World!\n" << std::endl;
+    std::cout << "Hi there!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
