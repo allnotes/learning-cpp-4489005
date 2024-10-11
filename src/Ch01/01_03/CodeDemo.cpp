223e9921edf3a1f55d442bdeaf55d3ddff944a54
Juan J Garcia <gtechjg@gmail.com>
2024-10-11 20:32:56 +0000

    Added comments and new syntax

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -5,11 +5,12 @@
 #include <iostream>
 #include <string>
 
-int main(){
+auto main() -> int {
     std::string str;
+    std::cout << "Please enter your name: " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
-    return (0);
+    //return (0);
 }
