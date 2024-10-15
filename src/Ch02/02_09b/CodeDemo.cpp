40dc771e72c027ccea50ae0eb178ac1e23b3690b
Juan J Garcia <gtechjg@gmail.com>
2024-10-15 21:48:30 +0000

    Added my syntax and comments

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,9 +3,27 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>//THE LETTER 'C' MEANS IT'S PART OF THE C STD LIBRARY
 
-int main(){
-    
+auto main() -> int{
+//int main(){
+    const size_t LENGTH1 = 25;
+    //TWO CHARACTER ARRAYS AS IN C
+    char array_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up?";
+
+    //TWO STRING OBJECTS AS IN C++
+    std::string std_str1 = "Hi everybody! ";
+    std::string std_str2 = "How's it going?";
+
+    //CONCATENATING TWO CHARACTER ARRAYS - strncat or strcat_s (for WINDOWS) //FAMILY FUNCTIONS
+    strncat(array_str1,array_str2,LENGTH1);//FIRST PART THE DESTINATION STRING
+    std::cout << array_str1 << std::endl;
+
+    //CONCATENATING TWO STRING OBJECTS - BOTH STRINGS REMAIN INTACT AFTER CONCAT
+    std::cout << std_str1 + std_str2 << std::endl;
+
     std::cout << std::endl << std::endl;
-    return (0);
+    //return (0);
 }
