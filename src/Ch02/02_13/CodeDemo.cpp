45d2c1e1d43cc87cef49b55a0b63b846ee79e74a
Juan J Garcia <gtechjg@gmail.com>
2024-10-16 21:27:32 +0000

    Added my syntax and comments

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -4,14 +4,17 @@
 
 #include <iostream>
 
-int main(){
+auto main() -> int{
+//int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
     // Write your code here
+    result = (float)(nums[0] + nums[1] +nums[2] +nums[3] + nums[4])/5;
+    //result /=5;
     
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
-    return (0);
+    //return (0);
 }
