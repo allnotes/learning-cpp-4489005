97675a7d634be606a87192c727410b0c0fa4aedf
Juan J Garcia <gtechjg@gmail.com>
2024-10-17 14:55:41 +0000

    Added my syntax and comments

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,23 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    
+enum class cow_purpose {dairy,meat,hide,pet};
+
+struct cow{
+    std::string name;
+    int age;
+    cow_purpose purpose;
+
+};
+
+auto main() -> int{
+//int main(){
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Betsy";
+    my_cow.purpose = cow_purpose::dairy;//THE STRING "dairy" IS NOT ASSIGNED SINCE THIS IS JUST A NUMBER
+    std::cout << my_cow.name << "is a type - " << (int)my_cow.purpose << " cow." <<std::endl;
+    std::cout << my_cow.name << " is " << my_cow.age << " years old." <<std::endl;
     std::cout << std::endl << std::endl;
-    return (0);
+    //return (0);
 }
