997cc098e6fb2aa4f0705e5cf414f9e3bad55c10
Juan J Garcia <gtechjg@gmail.com>
2024-10-18 20:28:03 +0000

    Addec comments and syntax

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -5,10 +5,21 @@
 #include <iostream>
 #include <vector>
 
-int main(){
+auto main() -> int{
+//int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     float average;
-    
+    average = 0.0f;
+    for (int i = 0; i < numbers.size(); i++)
+        average += numbers[i];
+    average /= numbers.size();
+    std::cout << "Average: " << average << std::endl;
+
+    average = 0.0f;
+    for (auto x: numbers)//WARNING: THE ELEMENT IS COPIED INTO THE VARIABLE EACH TIME SO IT COULD BE A BIG MEMORY DRAIN - YOU CAN USE A REFERENCE
+        average += x;
+    average /= numbers.size();
+    std::cout << "Average: " << average << std::endl;
     std::cout << std::endl << std::endl;
-    return (0);
+    //return (0);
 }
